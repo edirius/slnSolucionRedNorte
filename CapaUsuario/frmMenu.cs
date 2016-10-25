3a0c60f4746327414a1341da3115a5fbb0e945b9
Bryans Willians Huaman Yauri <softnyx_team_gm@hotmail.com>
2016-10-25 17:00:22 -0500

    Coneccion Base de Datos

---
+++ CapaUsuario/frmMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaUsuario.Properties;
+using CapaDatos;
+using CapaDeDatos;
+
+namespace CapaUsuario
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+            HacerConeccion();
+        }
+        public void HacerConeccion()
+        {
+            try
+            {
+                Conexion.IniciarSesion(Settings.Default.ConexionMySql, "bdPersonal", "root", "root");
+                MessageBox.Show(String.Format("{0}", "Se conecto exitosamente"));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+
+}
