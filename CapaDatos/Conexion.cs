83d882ca2c1fb157d4cabedcb2fd3122912c9786
Edward <Edward@HP-EDIRIUS>
2016-10-25 16:44:48 -0500

    Tercera Creacion

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDeDatos
+{
+    public class Conexion
+    {
+        public static GDatos GDatos;
+        public static bool IniciarSesion(string nombreServidor, string baseDatos, string usuario, string password)
+        {
+            GDatos = new MySqlServer(nombreServidor, baseDatos, usuario, password);
+            return GDatos.Autenticar();
+        } //fin inicializa sesion
+ 
+        public static void FinalizarSesion()
+        {
+            GDatos.CerrarConexion();
+        } //fin FinalizaSesion
+ 
+    }//end class util
+    }
+
---
+++ CapaDatos/cBaseDeDatos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace CapaDeDatos
+{
+
+    public class cBaseDeDatos
+    {
+       MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder(); 
+        
+        public void Conectar()
+        {
+            builder.Server = "localhost";
+            builder.UserID = "root";
+            builder.Password = "root";
+            builder.Database = "bdpersonal";
+        }
+        
+    }
+}
