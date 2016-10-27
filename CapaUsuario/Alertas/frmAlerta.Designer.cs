15052d8fc2f3549cf5c2a80be25dadc627214be7
Edward <Edward@HP-EDIRIUS>
2016-10-27 18:11:12 -0500

    Creacion de ALertas

---
+++ CapaUsuario/Alertas/frmAlerta.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CapaUsuario.Alertas
+{
+    partial class frmAlerta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lnkGestantesControlPreNatal = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ALERTAS";
+            // 
+            // lnkGestantesControlPreNatal
+            // 
+            this.lnkGestantesControlPreNatal.AutoSize = true;
+            this.lnkGestantesControlPreNatal.Location = new System.Drawing.Point(56, 80);
+            this.lnkGestantesControlPreNatal.Name = "lnkGestantesControlPreNatal";
+            this.lnkGestantesControlPreNatal.Size = new System.Drawing.Size(239, 13);
+            this.lnkGestantesControlPreNatal.TabIndex = 1;
+            this.lnkGestantesControlPreNatal.TabStop = true;
+            this.lnkGestantesControlPreNatal.Text = "GESTANTES QUE NO ACUDIERON A SU CITA";
+            // 
+            // frmAlerta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 446);
+            this.Controls.Add(this.lnkGestantesControlPreNatal);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmAlerta";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmAlerta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel lnkGestantesControlPreNatal;
+    }
+}
---
+++ CapaUsuario/Alertas/frmGestantesControlPreNatal.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CapaUsuario.Alertas
+{
+    partial class frmGestantesControlPreNatal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgListaGestantes = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgListaGestantes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgListaGestantes
+            // 
+            this.dtgListaGestantes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgListaGestantes.Location = new System.Drawing.Point(27, 51);
+            this.dtgListaGestantes.Name = "dtgListaGestantes";
+            this.dtgListaGestantes.Size = new System.Drawing.Size(556, 218);
+            this.dtgListaGestantes.TabIndex = 0;
+            // 
+            // frmGestantesControlPreNatal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 316);
+            this.Controls.Add(this.dtgListaGestantes);
+            this.Name = "frmGestantesControlPreNatal";
+            this.Text = "Relacion de Gestantes que no acuden a las citas.";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgListaGestantes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgListaGestantes;
+    }
+}
