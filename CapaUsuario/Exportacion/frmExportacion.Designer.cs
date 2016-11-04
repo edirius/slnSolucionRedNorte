9c13744d6a0330a38ef44ba30c42365048c46199
Edward <Edward@HP-EDIRIUS>
2016-11-04 16:33:18 -0500

    exportacion

---
+++ CapaUsuario/Exportacion/frmExportacion.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace CapaUsuario.Exportacion
+{
+    partial class frmExportacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblEstablecimiento = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblObstetra = new System.Windows.Forms.Label();
+            this.dlgGuardar = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Location = new System.Drawing.Point(367, 27);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(213, 170);
+            this.btnExportar.TabIndex = 0;
+            this.btnExportar.Text = "EXPORTAR";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ESTABLECIMIENTO DE SALUD:";
+            // 
+            // lblEstablecimiento
+            // 
+            this.lblEstablecimiento.AutoSize = true;
+            this.lblEstablecimiento.Location = new System.Drawing.Point(239, 46);
+            this.lblEstablecimiento.Name = "lblEstablecimiento";
+            this.lblEstablecimiento.Size = new System.Drawing.Size(35, 13);
+            this.lblEstablecimiento.TabIndex = 2;
+            this.lblEstablecimiento.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "OBSTETRA :";
+            // 
+            // lblObstetra
+            // 
+            this.lblObstetra.AutoSize = true;
+            this.lblObstetra.Location = new System.Drawing.Point(242, 86);
+            this.lblObstetra.Name = "lblObstetra";
+            this.lblObstetra.Size = new System.Drawing.Size(35, 13);
+            this.lblObstetra.TabIndex = 4;
+            this.lblObstetra.Text = "label3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 260);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(652, 146);
+            this.textBox1.TabIndex = 5;
+            // 
+            // frmExportacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 471);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblObstetra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblEstablecimiento);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExportar);
+            this.Name = "frmExportacion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exportar Datos";
+            this.Load += new System.EventHandler(this.frmExportacion_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblEstablecimiento;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblObstetra;
+        private System.Windows.Forms.SaveFileDialog dlgGuardar;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ CapaUsuario/Exportacion/frmExportacion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using CapaDeNegocios;
+using CapaDeNegocios.Exportacion;
+namespace CapaUsuario.Exportacion
+{
+    public partial class frmExportacion : Form
+    {
+        public frmExportacion()
+        {
+            InitializeComponent();
+        }
+
+        cExportar oExportar = new cExportar();
+
+        public string NombreObstetra { get; set; }
+        public string IdObstetra { get; set; }
+        public string NombreEstablecimientoSalud { get; set; }
+        public string IdEstablecimientoSalud { get; set; }
+
+        private void frmExportacion_Load(object sender, EventArgs e)
+        {
+            lblEstablecimiento.Text = NombreEstablecimientoSalud;
+            lblObstetra.Text = NombreObstetra;
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle,
+                                                                       Color.DarkBlue,
+                                                                       Color.Aquamarine,
+                                                                       45F))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            //dlgGuardar.Filter = "Archivos de Exportacion de GESSYS (*.gsys)|*.gsys";
+            //dlgGuardar.DefaultExt = ".gsys";
+            //dlgGuardar.ShowDialog();
+            //MessageBox.Show(dlgGuardar.FileName);
+
+            List<string> Caracteres = new List<string>();
+            Caracteres = oExportar.TraerDatosTabla("tobstetra");
+            for (int i = 0; i < Caracteres.Count; i++)
+            {
+                textBox1.Text = textBox1.Text + Caracteres[i] + System.Environment.NewLine;
+            }
+            
+        }
+    }
+}
