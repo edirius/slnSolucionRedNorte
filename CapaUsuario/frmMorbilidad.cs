bdf91eaa350179a21c96930c5b47e1661b9bb204
Guillermo Avendaño Vargas <guille.avendano@hotmail.com>
2016-11-08 17:07:30 -0500

    hc, mor

--- CapaUsuario/frmMorbilidad.cs
+++ CapaUsuario/frmMorbilidad.cs
@@ -22,9 +22,13 @@ namespace CapaUsuario
         public string DNI { get; set; }
         public string Nomnbre_Completo { get; set; }
         public string Edad { get; set; }
+
         public string IdEstablecimiento { get; set; }
+        CapaUsuario.frmHistoriaClinica fHC = new CapaUsuario.frmHistoriaClinica("","");
         int i = 0;
-    
+        DataTable odtGM = new DataTable();
+        
+
         private void frmMorbilidad_Load(object sender, EventArgs e)
         {
 
@@ -44,8 +48,9 @@ namespace CapaUsuario
         private void frmMorbilidad_Load_1(object sender, EventArgs e)
         {
             DataGridViewButtonColumn dgvbMorbilidad = new DataGridViewButtonColumn();
+            DataGridViewButtonColumn dgvbGestanteMorbilidad = new DataGridViewButtonColumn();
             CapaDeNegocios.cMorbilidad oMorbilidad = new CapaDeNegocios.cMorbilidad();
-
+            DataTable odtMorbilidad = new DataTable();
 
             txtHistoriaClinica.Text = Codigo_Historia_Clinica;
             dtpFecha.Value = Convert.ToDateTime(Fecha);
@@ -62,7 +67,11 @@ namespace CapaUsuario
             int columnIndex =4;
 
             oMorbilidad.Idtestablecimientosalud = IdEstablecimiento;
-            dgvMorbilidad_.DataSource = oMorbilidad.ListarMorbilidad();
+
+            odtMorbilidad = fHC.enumerar_datatable(oMorbilidad.ListarMantenimientoMorbilidad(), 0);
+
+            
+            dgvMorbilidad_.DataSource = odtMorbilidad ;
             dgvMorbilidad_.Columns.Insert(columnIndex, dgvbMorbilidad);
             //dgvMorbilidad_.Columns[2].Visible = false;
             dgvMorbilidad_.Columns[0].Width= 40;
@@ -71,6 +80,25 @@ namespace CapaUsuario
 
             cbFiltrar.SelectedItem = cbFiltrar.Items[0];
             txtBuscar_.Focus();
+            
+
+            odtGM.Columns.Add("N°", typeof(string));
+            odtGM.Columns.Add("Id Morbilidad", typeof(string));
+            odtGM.Columns.Add("Fecha", typeof(string));
+            odtGM.Columns.Add("Descripción", typeof(string));
+
+            /* Boton eliminar en dgvGestanteMorbilidad */
+
+            dgvbGestanteMorbilidad.Name = "Acción";
+            dgvbGestanteMorbilidad.Text = "Eliminar";
+            dgvbGestanteMorbilidad.HeaderText = "Acción";
+            dgvbGestanteMorbilidad.UseColumnTextForButtonValue = true;
+
+            
+            int columnIndex2 = 4;
+            dgvGestanteMorbildad.DataSource = odtGM;
+            dgvGestanteMorbildad.Columns.Insert(columnIndex2, dgvbGestanteMorbilidad);
+            
         }
 
         private void cbFiltrar_SelectedIndexChanged(object sender, EventArgs e)
@@ -80,26 +108,31 @@ namespace CapaUsuario
 
             if (seleccionado == "Todo") {
                 oMorbilidad.Idtestablecimientosalud = IdEstablecimiento;
-                dgvMorbilidad_.DataSource = oMorbilidad.ListarMorbilidad();
+                dgvMorbilidad_.DataSource = fHC.enumerar_datatable(oMorbilidad.ListarMantenimientoMorbilidad(),0);
                 dgvMorbilidad_.Columns[2].Visible = false;
+
+ 
             }
 
             if (seleccionado == "Extrema")
             {
                 oMorbilidad.Idtestablecimientosalud = IdEstablecimiento;
                 oMorbilidad.Tipo = seleccionado;
-                dgvMorbilidad_.DataSource = oMorbilidad.ListarMorbilidadXTipo();
+                dgvMorbilidad_.DataSource = fHC.enumerar_datatable( oMorbilidad.ListarMorbilidadXTipo(),0);
                 dgvMorbilidad_.Columns[2].Visible = false;
+ 
             }
 
             if (seleccionado == "Potencial")
             {
                 oMorbilidad.Idtestablecimientosalud = IdEstablecimiento;
                 oMorbilidad.Tipo = seleccionado;
-                dgvMorbilidad_.DataSource = oMorbilidad.ListarMorbilidadXTipo();
+                dgvMorbilidad_.DataSource = fHC.enumerar_datatable( oMorbilidad.ListarMorbilidadXTipo(),0);
                 dgvMorbilidad_.Columns[2].Visible = false;
+ 
             }
 
+            
         }
 
         private void txtBuscar__TextChanged(object sender, EventArgs e)
@@ -110,14 +143,14 @@ namespace CapaUsuario
             if (buscar.Length > 1) {
                 oMorbilidad.Idtestablecimientosalud = IdEstablecimiento;
                 oMorbilidad.Descripcion = buscar;
-                dgvMorbilidad_.DataSource = oMorbilidad.BuscarMorbilidad();
+                dgvMorbilidad_.DataSource = fHC.enumerar_datatable( oMorbilidad.BuscarMorbilidad(),0);
             }
 
             if (buscar.Length == 0)
             {
                 oMorbilidad.Idtestablecimientosalud = IdEstablecimiento;
                 oMorbilidad.Descripcion = buscar;
-                dgvMorbilidad_.DataSource = oMorbilidad.ListarMorbilidad();
+                dgvMorbilidad_.DataSource = fHC.enumerar_datatable( oMorbilidad.ListarMantenimientoMorbilidad(),0);
             }
 
 
@@ -125,36 +158,52 @@ namespace CapaUsuario
 
         private void btnNuevo_Click(object sender, EventArgs e)
         {
-
-            dgvPacienteMorbilidad.Rows.Clear();
-            txtBuscar.Text = "";
-            txtBuscar.Focus();
-
-
+            odtGM.Rows.Clear();
+            dgvGestanteMorbildad.DataSource = odtGM;
+            txtBuscar_.Text = "";
+            i = 0;
+            dtpFechaMorbilidad.Focus();
         }
 
         private void agregar_gestante_morbilidad(DataGridView dgvGM, DataGridView dgvM, int posicion ) {
-            DataTable odtGM = new DataTable();
+
+
+            /* "N°" , "Id Morbilidad" , "Fecha","Descripción" */
             DataRow drGM = odtGM.NewRow();
-
-            odtGM.Columns.Add("N°", typeof(string));
-            odtGM.Columns.Add("Id Morbilidad", typeof(string));
-            odtGM.Columns.Add("Descripción", typeof(string));
-
             drGM = odtGM.NewRow();
+            string a = "";
+            string b = "";
+            bool bandera_agregar = false;
 
-            //i = odtGM.Rows.Count;
-            i++;
-            drGM[0] = i;
-            drGM[1] = dgvM[0, posicion].Value.ToString(); ;
-            drGM[2] = dgvM[1, posicion].Value.ToString(); ;
+            for (int i=0; i < dgvGM.Rows.Count; i++) {
+                a = dgvM[2, posicion].Value.ToString();
+                b = dgvGM[1, i].Value.ToString();
+                if (a.Trim() == b.Trim()) { 
+                    MessageBox.Show("Morbilidad de gestante ya agregada.", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    bandera_agregar = true;
+                }
+            }
 
-            odtGM.Rows.InsertAt(drGM, i);
-            dgvGM.DataSource = odtGM;
+            if (!bandera_agregar){ 
+                i++;
+                drGM[0] = i;
+                drGM[1] = dgvM[2, posicion].Value.ToString(); 
+                drGM[2] = dtpFechaMorbilidad.Value ;
+                drGM[3] = dgvM[3, posicion].Value.ToString(); 
+
+
+                odtGM.Rows.InsertAt(drGM, i);
+                dgvGM.DataSource = odtGM;
+
+                dgvGM.Columns[1].Visible = false;
+                dgvGM.Columns[0].Width = 60;
+                dgvGM.Columns[2].Width = 120;
+            }
+
             //dtpEcografia.Focus();
         }
 
-        private void dgvMorbilidad__CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void dgvMorbilidad__CellContentClick_1(object sender, DataGridViewCellEventArgs e)
         {
             if (dgvMorbilidad_.CurrentCell.ColumnIndex == 0)
             {
@@ -162,5 +211,78 @@ namespace CapaUsuario
                 
             }
         }
+
+        private void dgvGestanteMorbildad_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvGestanteMorbildad.CurrentCell.ColumnIndex == 4)
+            {
+
+                int indice = e.RowIndex + 1;
+
+                if (MessageBox.Show("Está seguro que desea eliminar la morbilidad Nº " + indice, "Confirmar Eliminar", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    //dgvOdontologia.Rows.RemoveAt(e.RowIndex);
+                    odtGM.Rows.RemoveAt(e.RowIndex);
+
+                    for (int i = 0; i < odtGM.Rows.Count; i++)
+                    {
+                        odtGM.Rows[i][0] = i + 1;
+                    }
+
+                    dgvGestanteMorbildad.DataSource = odtGM;
+                    dgvGestanteMorbildad.Columns[0].Width = 60;
+                    dgvGestanteMorbildad.Columns[2].Width = 120;
+                    return;
+                }
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            CapaDeNegocios.cGestanteMorbilidad oGestanteMorbilidad = new CapaDeNegocios.cGestanteMorbilidad();
+            DataTable odtGestanteMorbilidad = new DataTable();
+
+            oGestanteMorbilidad.idthistoriaclinica = Codigo_Historia_Clinica;
+            oGestanteMorbilidad.EliminarGestanteMorbilidad();
+
+            for (int i = 0; i < dgvGestanteMorbildad.Rows.Count; i++)
+            {
+                oGestanteMorbilidad.idtestablecimientosalud = IdEstablecimiento;
+                oGestanteMorbilidad.idtmorbilidad = dgvGestanteMorbildad.Rows[i].Cells[1].Value.ToString();
+                oGestanteMorbilidad.idthistoriaclinica = Codigo_Historia_Clinica;
+                oGestanteMorbilidad.fecha = Convert.ToDateTime(dgvGestanteMorbildad.Rows[i].Cells[2].Value.ToString() );
+                odtGestanteMorbilidad = oGestanteMorbilidad.CrearGestanteMorbilidad();
+
+                foreach (DataRow row in odtGestanteMorbilidad.Rows)
+                {
+
+                    string respuesta_historia_clinica = row[0].ToString().Trim();
+
+                    string[] words = respuesta_historia_clinica.Split('*');
+
+                    string exito = words[0].ToString();
+                    string respuesta = words[1].ToString();
+                    //string idthistoriaclinica = words[2].ToString();
+
+                    if (exito == "1")
+                        MessageBox.Show(respuesta, "Atención", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+        }
+
+        private void dtpFechaMorbilidad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+                btnGuardar.Focus();
+        }
+
+        private void txtBuscar__KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+                dtpFechaMorbilidad.Focus();
+        }
+
+   
     }
 }
