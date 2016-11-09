042bd3f7b7d7732357c561ee6f001adbfd6eae78
Bryans Willians Huaman Yauri <softnyx_team_gm@hotmail.com>
2016-11-09 16:15:09 -0500

    Menu

--- CapaUsuario/Properties/Resources.Designer.cs
+++ CapaUsuario/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace CapaUsuario.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap FondoNorte {
+            get {
+                object obj = ResourceManager.GetObject("FondoNorte", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap margen {
             get {
                 object obj = ResourceManager.GetObject("margen", resourceCulture);
