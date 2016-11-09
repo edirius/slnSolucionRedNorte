f7f77dbfdb224fc905f51122f8383c13fe7a9c4b
OMAR ESTEBAN SICCOS ESTRADA <oese322@hotmail.com>
2016-11-09 14:36:56 -0500

    09/11/2016

--- CapaUsuario/Alertas/rptAlertaControlPuerperio.cs
+++ CapaUsuario/Alertas/rptAlertaControlPuerperio.cs
@@ -16,14 +16,14 @@ namespace CapaUsuario.Alertas {
     using CrystalDecisions.CrystalReports.Engine;
     
     
-    public class rptListaGestantesControlPreNatal : ReportClass {
+    public class rptAlertaControlPuerperio : ReportClass {
         
-        public rptListaGestantesControlPreNatal() {
+        public rptAlertaControlPuerperio() {
         }
         
         public override string ResourceName {
             get {
-                return "rptListaGestantesControlPreNatal.rpt";
+                return "rptAlertaControlPuerperio.rpt";
             }
             set {
                 // Do nothing
@@ -41,7 +41,7 @@ namespace CapaUsuario.Alertas {
         
         public override string FullResourceName {
             get {
-                return "CapaUsuario.Alertas.rptListaGestantesControlPreNatal.rpt";
+                return "CapaUsuario.Alertas.rptAlertaControlPuerperio.rpt";
             }
             set {
                 // Do nothing
@@ -66,7 +66,7 @@ namespace CapaUsuario.Alertas {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section Section3 {
+        public CrystalDecisions.CrystalReports.Engine.Section GroupHeaderSection1 {
             get {
                 return this.ReportDefinition.Sections[2];
             }
@@ -74,7 +74,7 @@ namespace CapaUsuario.Alertas {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section Section4 {
+        public CrystalDecisions.CrystalReports.Engine.Section Section3 {
             get {
                 return this.ReportDefinition.Sections[3];
             }
@@ -82,17 +82,49 @@ namespace CapaUsuario.Alertas {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section Section5 {
+        public CrystalDecisions.CrystalReports.Engine.Section GroupFooterSection1 {
             get {
                 return this.ReportDefinition.Sections[4];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.CrystalReports.Engine.Section Section4 {
+            get {
+                return this.ReportDefinition.Sections[5];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.CrystalReports.Engine.Section Section5 {
+            get {
+                return this.ReportDefinition.Sections[6];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_pidtobstetra {
+            get {
+                return this.DataDefinition.ParameterFields[0];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_pidtestablecimientosalud {
+            get {
+                return this.DataDefinition.ParameterFields[1];
             }
         }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
-    public class CachedrptListaGestantesControlPreNatal : Component, ICachedReport {
+    public class CachedrptAlertaControlPuerperio : Component, ICachedReport {
         
-        public CachedrptListaGestantesControlPreNatal() {
+        public CachedrptAlertaControlPuerperio() {
         }
         
         [Browsable(false)]
@@ -129,7 +161,7 @@ namespace CapaUsuario.Alertas {
         }
         
         public virtual CrystalDecisions.CrystalReports.Engine.ReportDocument CreateReport() {
-            rptListaGestantesControlPreNatal rpt = new rptListaGestantesControlPreNatal();
+            rptAlertaControlPuerperio rpt = new rptAlertaControlPuerperio();
             rpt.Site = this.Site;
             return rpt;
         }
