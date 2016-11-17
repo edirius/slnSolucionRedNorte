4bcd8b140ae04b32f7d60840d442c7155735ffa8
Guillermo Avendaño Vargas <guille.avendano@hotmail.com>
2016-11-17 15:41:33 -0500

    generar numero masculino

--- CapaDeNegocios/cUtilitarios.cs
+++ CapaDeNegocios/cUtilitarios.cs
@@ -100,6 +100,41 @@ namespace CapaDeNegocios
             }
         }
 
+        public string GenerarNumeroMasculino(int N)
+        {
+            String[] Unidad = { "", "PRIMER", "SEGUNDO", "TERCER",
+            "CUARTO", "QUINTO", "SEXTO", "SEPTIMO", "OCTAVO",
+            "NOVENO" };
+            String[] Decena = { "", "DECIMO", "VIGESIMO", "TRIGESIMO",
+            "CUADRAGESIMO", "QUINCUAGESIMO", "SEXAGESIMO",
+            "SEPTUAGESIMO", "OCTOGESIMO", "NONAGESIMO" };
+            String[] Centena = {"", "centesimo", "ducentesimo",
+            "tricentesimo", " cuadringentesimo", " quingentesimo",
+            " sexcentesimo", " septingentesimo", " octingentesimo",
+            " noningentesimo"};
+
+            string Num = "";
+            int u = N % 10;
+            int d = (N / 10) % 10;
+            int c = N / 100;
+            if (N >= 100)
+            {
+                Num = Centena[c] + " " + Decena[d] + " " + Unidad[u];
+            }
+            else
+            {
+                if (N >= 10)
+                {
+                    Num = Decena[d] + " " + Unidad[u];
+                }
+                else
+                {
+                    Num = Unidad[N];
+                }
+            }
+            return Num;
+        }
+
         public string GenerarNumero(int N)
         {
             String[] Unidad = { "", "PRIMERA", "SEGUNDA", "TERCERA",
